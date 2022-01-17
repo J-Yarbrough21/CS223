395211e9e8b19a3f5b3eac8cb2389bf46e3aa09f
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-16 22:10:03 -0800

    mod2

--- project2.py
+++ project2.py
@@ -2,62 +2,58 @@
 import tkinter as tk
 from tkinter import ttk
 from xml.dom import pulldom
+from tkinter import messagebox
 
 root = tk.Tk()
-root.geometry("900x400")
+root.geometry("375x400")
 root.title("Average Test Scores-Lab2")
 
 frame = ttk.Frame(root, padding="10 10 10 10")
 frame.pack(fill=tk.BOTH, expand=True)
 
-ttk.Label(frame, text="Student First Name").grid(column=0, row=0, sticky=tk.W)
+fnameLabel = ttk.Label(frame, text="Student First Name").grid(
+    column=0, row=0, sticky=tk.W
+)
 fnameText = tk.StringVar()
 fnameEntry = ttk.Entry(frame, width=25, textvariable=fnameText).grid(column=0, row=2)
 
-ttk.Label(frame, text="Student Last Name").grid(column=1, row=0, sticky=tk.W)
+lnameLabel = ttk.Label(frame, text="Student Last Name").grid(
+    column=1, row=0, sticky=tk.W
+)
 lnameText = tk.StringVar()
 lnameEntry = ttk.Entry(frame, width=25, textvariable=lnameText).grid(column=1, row=2)
 
-ttk.Label(frame, text="Score 1").grid(column=0, row=4, sticky=tk.W)
+score1Label = ttk.Label(frame, text="Score 1").grid(column=0, row=4, sticky=tk.W)
 score1Text = tk.StringVar()
 score1Entry = ttk.Entry(frame, width=25, textvariable=score1Text).grid(column=1, row=4)
 
-ttk.Label(frame, text="Score 2").grid(column=0, row=5, sticky=tk.W)
+score2Label = ttk.Label(frame, text="Score 2").grid(column=0, row=5, sticky=tk.W)
 score2Text = tk.StringVar()
 score2Entry = ttk.Entry(frame, width=25, textvariable=score2Text).grid(column=1, row=5)
 
-ttk.Label(frame, text="Score 3").grid(column=0, row=6, sticky=tk.W)
+score3Label = ttk.Label(frame, text="Score 3").grid(column=0, row=6, sticky=tk.W)
 score3Text = tk.StringVar()
 score3Entry = ttk.Entry(frame, width=25, textvariable=score3Text).grid(column=1, row=6)
 
-ttk.Label(frame, text="Student Full Name").grid(column=3, row=0, sticky=tk.W)
 fullnameText = tk.StringVar()
-fullnameLabel = ttk.Entry(
-    frame, width=25, textvariable=fullnameText, state="readonly"
-).grid(column=4, row=0)
-
-ttk.Label(frame, text="Average Score").grid(column=3, row=2, sticky=tk.W)
-averageText = tk.StringVar()
-averageLabel = ttk.Entry(
-    frame, width=25, textvariable=averageText, state="readonly"
-).grid(column=4, row=2)
-
-ttk.Label(frame, text="Total Score").grid(column=3, row=4, sticky=tk.W)
-totalText = tk.StringVar()
-totalLabel = ttk.Entry(frame, width=25, textvariable=totalText, state="readonly").grid(
-    column=4, row=4
+fullnameLabel = ttk.Label(frame, text="Student Full Name : ").grid(
+    column=0, row=8, sticky=tk.W
 )
+
+averageLabel = ttk.Label(frame, text="Average Score :").grid(
+    column=0, row=10, sticky=tk.W
+)
+averageText = tk.StringVar()
+
+totalLabel = ttk.Label(frame, text="Total Score :").grid(column=0, row=12, sticky=tk.W)
+totalText = tk.StringVar()
 
 
 def click_button1():
-    global fnameText
-    global lnameText
-    global fullnameText
-
-    first = str(fnameText.get)
-    last = str(lnameText.get)
+    first = fnameText.get
+    last = lnameText.get
     fullname = first + " " + last
-    fullnameText.set(fullname)
+    fullnameText.set = str(fullname)
 
 
 def click_button2():
@@ -65,10 +61,10 @@ def click_button2():
 
 
 button1 = ttk.Button(frame, text="Calculate", command=None)
-button1.grid(column=4, row=11)
+button1.grid(column=0, row=22)
 
 button2 = ttk.Button(frame, text="Clear", command=click_button2)
-button2.grid(column=2, row=11)
+button2.grid(column=1, row=22)
 
 for child in frame.winfo_children():
     child.grid_configure(padx=10, pady=10)
