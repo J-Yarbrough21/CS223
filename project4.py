a11ec561e5003c678385b284ade3633686433d34
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-18 01:32:51 -0800

    initial assignment

---
+++ project4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.geometry("980x450")
+window.title("Temperature Conversion")
+
+#the main conversion
+#window configuration
+#user instruction and input
+#shows result for opposite temp
+#allow button to be clicked to convert
+#add function for the button to convert from C to F 
+#bonus points for code to catch user input error
+
+
+window.mainloop()
