adee77e3f59fec2be2abdc25011fda0084322166
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-18 01:29:29 -0800

    initial assignment

---
+++ project3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.geometry("980x450")
+window.title("Future Value Calculator")
+
+# create the GUI
+# hard code the necessary buttons
+# properly define the functions
+# convert yearly values to montly values
+# calculate future value
+# call the functions
+
+
+window.mainloop()
