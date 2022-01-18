9b190123d2f36d14b2d8bd823c6a8b3c85383ca3
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-18 12:33:34 -0800

    mod6

--- project2.py
+++ project2.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from xml.dom import pulldom
 from tkinter import messagebox
 
+def init
 root = tk.Tk()
 root.geometry("375x400")
 root.title("Average Test Scores-Lab2")
@@ -35,39 +36,27 @@ score3Label = ttk.Label(frame, text="Score 3").grid(column=0, row=6, sticky=tk.W
 score3Text = tk.StringVar()
 score3Entry = ttk.Entry(frame, width=25, textvariable=score3Text).grid(column=1, row=6)
 
-fnameText = tk.StringVar()
-lnameText = tk.StringVar()
-score1Text = tk.StringVar()
-score2Text = tk.StringVar()
-score3Text = tk.StringVar()
+ttk.Label(frame, text="Student Full Name: ").grid(column=0, row=10, sticky=tk.E)
+ttk.Entry(frame, width=200, textvariable=self.full_name).grid(column=0, row=12)
 
-fullname_Label = tk.Label(frame, text=" ").grid(column=0, row=20)
+ttk.Label(frame, text="Average Score: ").grid(column=0, row=14, sticky=tk.E)
+ttk.Entry(frame, width=200, textvariable=self.average_score).grid(column=0, row=16)
 
-average_Label = tk.Label(frame, text=" ").grid(column=0, row=22)
-
-total_Label = tk.Label(frame, text=" ").grid(column=0, row=24)
-
+ttk.Label(frame, text="Total Score: ").grid(column=0, row=16, sticky=tk.E)
+ttk.Entry(frame, width=200, textvariable=self.total_score).grid(column=0, row=18) 
 
 def click_button1():
-    first = str(fnameText.get())
-    last = str(lnameText.get())
-    fullname = first + " " + last
-    display = "Student Full Name" + str(fullname)
-    fullname_Label.grid
-    fullname_Label["text"] = display
-
+  
 
 # fullnameText = fnameText+lnameText
 # totalText = score1Entry+score2Entry+score3Entry
 # averageText = round(totalText/3)
 # display=total and average of scores.
 
-
 def click_button2():
     root.destroy()
 
-
-button1 = ttk.Button(frame, text="Calculate", command=None)
+button1 = ttk.Button(frame, text="Score", command=None)
 button1.grid(column=0, row=30)
 
 button2 = ttk.Button(frame, text="Clear", command=click_button2)
--- project4.py
+++ project4.py
@@ -6,13 +6,13 @@ window = Tk()
 window.geometry("980x450")
 window.title("Temperature Conversion")
 
-#the main conversion
-#window configuration
-#user instruction and input
-#shows result for opposite temp
-#allow button to be clicked to convert
-#add function for the button to convert from C to F 
-#bonus points for code to catch user input error
+# the main conversion
+# window configuration
+# user instruction and input
+# shows result for opposite temp
+# allow button to be clicked to convert
+# add function for the button to convert from C to F
+# bonus points for code to catch user input error
 
 
 window.mainloop()
