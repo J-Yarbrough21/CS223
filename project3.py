43f836974e536eed811f66e7d7764939e9be57ec
Jennifer Yarbrough <nextjeninspections@icloud.com>
2022-01-19 23:24:21 -0800

    mod6

--- project3.py
+++ project3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from tkinter import *
-from tkinter import messagebox
 
 window = Tk()
 window.geometry("900x900")
@@ -18,10 +17,10 @@ while choice.lower() == "y":
     def calculate_future_value(monthly_investment, yearly_interest, years):
         # convert yearly values to monthly values
         monthly_interest_rate = yearly_interest / 12 / 100
-        months = years * 12
+        month = years * 12
 
         future_value = 0.0
-        for i in range(0, months):
+        for i in range(0, month):
             future_value += monthly_investment
             monthly_interest = future_value * monthly_interest_rate
             future_value += monthly_interest
@@ -34,21 +33,12 @@ def main():
 
         # Get input from user
         monthly_investment = float(input("Enter Monthly Investment Amount: \t"))
-    yearly_interest_rate = float(input("Enter Yearly Rate: \t"))
-    years = int(input("Enter Number of Years : \t\t"))
-
-    # convert yearly values to monthly values
-    monthly_interest_rate = yearly_interest_rate / 12 / 100
-    month = years * 12
+        yearly_interest_rate = float(input("Enter Yearly Interest Rate: \t"))
+        years = int(input("Enter Number of Years : \t\t"))
 
 
-# calculate the future values
-future_value = 0
-for i in range(month):
-    future_value += monthly_investment
-    monthly_interest_amount = future_value * monthly_interest_rate
-    future_value += monthly_interest_amount
-
+# Get future_value
+future_value = calculate_future_value(monthly_investment, yearly_interest_rate, years)
 
 # display the result
 print("Future value:\t\t\t" + str(round(future_value, 2)))
@@ -63,16 +53,14 @@ print("Bye")
 if main == "_main":
     main()
 
-
 # create the GUI
 # make the title your fake bank name ie JY Bank Future Value Calculator
 # hard code the interest rate to 9.5%
 # create the necessary buttons - y/n continue
 # properly define the functions
-# convert yearly values to montly values
+# convert yearly values to monthly values
 # calculate future value
 # call the functions
 # view like Exercise 3-3
-
 
 window.mainloop()
