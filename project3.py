34ac4164539cbaf426590c67277f04fe4e16e843
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-21 02:02:44 -0800

    Mod9

--- project3.py
+++ project3.py
@@ -32,25 +32,55 @@ yl_entry = Entry(root, font=("Times", 18))
 yl_entry.place(x=500, y=10, width=200, height=30)
 
 
-# PV = mi_entry.get
-# Years = yl_entry.get
-# Months = years * 12
-# n = Months
+def m_int(self):
+    int1 = 9.50 / 100
+    int2 = int1 / 12
+    m_int = int2
 
-# interest1 = 9.50/100 = annual rate converted to decimal
-# interest2 = interest1/12 = monthly interest rate
-# i = interest2
+
+def months1(self):
+    years = float(mi_entry.get())
+    months = years * 12
+    months1 = months
+
+
+def fut_val1():
+    mi_1 = float(mi_entry.get())
+    fv_1a = float(mi_1) * (1 + m_int)
+    fut_val1 = fv_1a
+
+
+def fut_val2():
+    mi_1 = float(mi_entry.get())
+    fv_1a = float(mi_1) * (1 + m_int)
+    fut_val2 = fv_1a
+
+
+def calculate():
+
+    # future Value formula FV=PV(1+m_int)to the "n" power
+    # FV1= (PV=mi_entry)(1+ i = interest2)
+    # FV2 = FV1 * FV2
+
+    # Out Put Boxes
+    # Yearly Future Value
+
+    root.mainloop()
+
 
 # future Value formula FV=PV(1+i)to the "n" power
 # FV1= (PV=mi_entry)(1+ i = interest2)
 # FV2 = FV1 * FV1
 
+# mi_1 = mi_entry.get
 
-# Out Put Boxes
-# Yearly Future Value
+# Years = yl_entry.get
+# Months = years * 12
+# fv_2 = Months
 
-
-root.mainloop()
+# interest1 = 9.50/100 = annual rate converted to decimal
+# interest2 = interest1/12 = monthly interest rate
+# m_int = interest2
 
 # simple interest = PxRxT P=Principal R=Rate T=Time
 # future Value formula FV=PV(1+i)to the "n" power
