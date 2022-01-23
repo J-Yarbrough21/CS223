e5fa296411b338655ed2f82debe1597c37c5f4db
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-01-22 20:57:33 -0800

    Mod8

--- project4.py
+++ project4.py
@@ -5,7 +5,7 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.geometry("980x900")
+root.geometry("700x300")
 root.title("Temperature Conversion")
 
 # USER INPUT
@@ -14,44 +14,58 @@ label = Label(
     root,
     text="Temperature in Celsius: ",
     font=("Times", 18, "bold"),
-    fg="#00FFFF",
     justify=("right"),
 )
 tc_string = StringVar()
-label.place(x=50, y=25, width=200, height=30)
-tc_entry = Entry(root, textvariable=tc_string, font=("Times", 18))
-tc_entry.place(x=250, y=25, width=200, height=30)
-
+label.place(x=155, y=50, width=200, height=30)
+tc_entry = Entry(root, textvariable=tc_string, font=("Times", 18, "bold"))
+tc_entry.place(x=350, y=50, width=200, height=30)
 
 # OUTPUT IN FAHRENHEIT
+
+label = Label(
+    root,
+    text=" Temperature in Fahrenheit:  ",
+    font=("Times", 18, "bold"),
+    justify=("right"),
+)
+tf_string = StringVar()
+label.place(x=115, y=125, width=250, height=30)
+tf_entry = Entry(root, textvariable=tf_string, font=("Times", 18))
+tf_entry.place(x=350, y=125, width=200, height=30)
 
 
 # FUNCTIONS
 
 
 def convert():
+    ct1 = float(tc_entry.get())
+    ct2 = float(ct1 * 1.8)
+    ct3 = float(ct2 + 32)
+    tf_string.set("{:.1f}".format(ct3))
 
     # Celsius to Fahrenheit Formula: (°C * 1.8) + 32 = °F
 
     # Fahrenheit to Celsius Formula: (°F - 32) / 1.8 = °C
 
-    # def clear():
-    #   mi_string.set("")
-    #   yl_string.set("")
-    #   yfv_string.set("")
 
-    def kill():
-        root.destroy
+def clr():
+    tc_string.set("")
+    tf_string.set("")
 
 
-c2f = Button(root, text="Convert C to F, command=convert", font=("Times", 12, "bold"))
-c2f.place(x=25, y=200, width=150, height=30)
+def destroy():
+    root.destroy()
 
-# clear = Button(root, text="Clear", command=clear, font=("Times", 12, "bold"))
-# clear.place(x=225, y=200, width=150, height=30)
 
-exit = Button(root, text="Exit", command=kill, font=("Times", 12, "bold"))
-exit.place(x=425, y=200, width=150, height=30)
+c2f = Button(root, text="Convert ºC to ºF", command=convert, font=("Times", 18, "bold"))
+c2f.place(x=75, y=200, width=150, height=30)
+
+clr = Button(root, text="Clear", command=clr, font=("Times", 18, "bold"))
+clr.place(x=275, y=200, width=150, height=30)
+
+exit = Button(root, text="Exit", command=destroy, font=("Times", 18, "bold"))
+exit.place(x=475, y=200, width=150, height=30)
 
 root.mainloop()
 
