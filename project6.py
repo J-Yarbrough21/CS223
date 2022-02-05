980dfd322b2f7f90cd1ba2fa9b5ddad9662c61c3
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-02-05 10:53:50 -0800

    initial program

---
+++ project6.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+# Please complete the exercises in your textbook 6-2 page 200
+
+
+# use the list for th Test Scores program
+# add two more movies to the list
+# turn in your working py code
+# completed project
+# terminate at 5 places
