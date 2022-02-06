eced99605edd7135aa518922c40e44603ee5a371
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-02-05 19:57:48 -0800

    Mod1

--- project5.py
+++ project5.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
-
+# Establishes the name of the program, allows for exit of the program
 def display_welcome():
     print("The Test Scores Program")
     print("Enter 'x' to exit")
     print("")
 
 
+# makes scores a list, recieves input from user, checks for validity of entry
 def get_scores():
-    score_total = 0
-    counter = 0
+    scores = []
     while True:
         score = input("Enter test score: ")
         if score == "x":
-            return score_total, counter
+            return scores
         else:
             score = int(score)
             if score >= 0 and score <= 100:
-                score_total += score
-                counter += 1
+                scores.append(score)
             else:
                 print(
                     "Test score must be from 0 through 100."
@@ -26,21 +25,34 @@ def get_scores():
                 )
 
 
-def process_scores(score_total, count):
-    # calculate average score
-    average = score_total / count
+def length(get_scores):
+    global t_length
+    t_length = 0
+    for how_many in get_scores:
+        t_length += 1
+        return t_length
 
-    # format and display the result
+
+def process_scores(scores):  # calculations for total, average, low, high and median
+
+    # average = scores / length
+    low_score = min(scores)
+    high_score = max(scores)
+    # median = #middle number of the 5 scores
+
     print()
-    print("Score total:     ", score_total)
-    print("Number of Scores:   ", count)
-    print("Average Score:   ", average)
+    print("Score total:     ", scores)
+    print("Number of Scores:   ", t_length)
+    # print("Average Score:   ", average)
+    print("Low Score:    ", low_score)
+    print("High Score:  ", high_score)
+    # print("Median Score:  ", median)
 
 
 def main():
     display_welcome()
-    score_total, count = get_scores()
-    process_scores(score_total, count)
+    scores = get_scores()
+    process_scores(scores)
     print("")
     print("Bye")
 
