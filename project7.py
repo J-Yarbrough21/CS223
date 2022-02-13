146fb0b606906da596a6dbf56827e448da34d80e
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-02-12 22:22:37 -0800

    Mod5

--- project7.py
+++ project7.py
@@ -14,37 +14,30 @@ s_list = [
 p_list = [2.50, 3.49, 9.95, 5.99, 3.99, 1.99, 3.99, 7.99]
 print("s_list p_list")
 
-# Create and print a Parallel Array that matches each item to its price from both lists.
-# should look like this... 1. Milk $2.99
-# Print the array list with its index numbers.
 for i in range(len(s_list)):
     print(s_list[i]), " ", "$", (p_list[i])
 
-
 for index, i in enumerate(s_list):
     print(index, i)
-
     print("Select item(s) from shopping list.")
-    for i in range(1):
-        index = int(input("Enter your item number: "))
-        print(s_list[index], "$", p_list[index])
-    # Select one or more items from the shopping list using the index number.
-    # Print your selected items with price
-    # Print sub total (base price of items before tax)
-    for items in p_list:
-        subtotal = p_list[index]
-        print("sub total")
-        print("$" + str(float(p_list[index])))
+
+for i in range(1):
+    index = int(input("Enter your item number: "))
+    print(s_list[index], "$", p_list[index])
+
+for items in p_list:
+    subtotal = p_list[index]
+    print("sub total")
+    print("$" + str(float(p_list[index])))
 
 
-# Create three global variables with the proper data types
-def s_tax():  # Sales tax is 8.9% - 0.089 - 8.9/100
+def s_tax():
     global tax1
     tax1 = 8.9 / 100
     return tax1
 
 
-def sub_total():  # sub_total #total of items
+def sub_total():
     global sb_1
     sb_1 = subtotal
     return sb_1
@@ -53,18 +46,10 @@ def sub_total():  # sub_total #total of items
 print("$", round(sub_total(), 2))
 
 
-def sub_tax_total():  # sub_total2 # total of items * tax = tax total
+def sub_tax_total():
     global stt
     stt = sub_total * s_tax
     return stt
 
 
-print(
-    "$", round(sub_tax_total(), 2)
-)  # Print sales tax amount (before adding it into total price)
-
-# total_price # sub_total2 + sub_total = total amount
-
-
-# Calculate and print total price with tax included.
-# Round to two decimal points
+print("$", round(sub_tax_total(), 2))
