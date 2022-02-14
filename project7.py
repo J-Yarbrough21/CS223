2e756e3eaf2070c3741e095aa33fbfa44f9e277a
J-Yarbrough21 <nextjeninspections@icloud.com>
2022-02-13 16:37:10 -0800

    Final

--- project7.py
+++ project7.py
@@ -12,23 +12,15 @@ s_list = [
     "Shashyk",
 ]
 p_list = [2.50, 3.49, 9.95, 5.99, 3.99, 1.99, 3.99, 7.99]
-print("s_list p_list")
 
-for i in range(len(s_list)):
-    print(s_list[i]), " ", "$", (p_list[i])
+shopping_cart = []
+subtotal = 0
 
-for index, i in enumerate(s_list):
-    print(index, i)
-    print("Select item(s) from shopping list.")
 
-for i in range(1):
-    index = int(input("Enter your item number: "))
-    print(s_list[index], "$", p_list[index])
-
-for items in p_list:
-    subtotal = p_list[index]
-    print("sub total")
-    print("$" + str(float(p_list[index])))
+def sub_tax_total():
+    global stt
+    stt = subtotal * s_tax()
+    return stt
 
 
 def s_tax():
@@ -37,19 +29,26 @@ def s_tax():
     return tax1
 
 
-def sub_total():
-    global sb_1
-    sb_1 = subtotal
-    return sb_1
+def main(sub_tax_total, s_tax):
+    global subtotal
+
+    print("Select item(s) from shopping list.")
+    for index, i in enumerate(s_list):
+        print("{}. {:<32} {:.2f}".format(index + 1, i, p_list[index]))
+
+    for i in range(1):
+        index = int(input("Enter your item number: "))
+        print(s_list[index], "$", p_list[index])
+        shopping_cart.append(index)
+        print("Your current cart is: ")
+
+        for item in shopping_cart:
+            print(s_list[item])
+            subtotal += p_list[item]
+            print("Sub Total: ${:.2f}".format(round(subtotal, 2)))
+            print("Sales Tax: ${:.2f}".format(round(sub_tax_total(), 2)))
+            print("Total: ${:.2f}".format(round(subtotal + sub_tax_total(), 2)))
 
 
-print("$", round(sub_total(), 2))
-
-
-def sub_tax_total():
-    global stt
-    stt = sub_total * s_tax
-    return stt
-
-
-print("$", round(sub_tax_total(), 2))
+if __name__ == "__main__":
+    main(sub_tax_total, s_tax)
